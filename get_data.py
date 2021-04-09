67e75fab5670a9eeddf9a8484d41080d0ce9fa50
CharlieJWMarsh <72220190+CharlieJWMarsh@users.noreply.github.com>
2021-04-09 13:34:37 +0100

    Got regions each tweet is picked from as location information is often insufficient

--- get_data.py
+++ get_data.py
@@ -3,6 +3,14 @@ import tweepy as tw
 import pandas as pd
 import numpy as np
 
+import geopandas as gpd
+import geopy
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+import certifi
+import ssl
+import geopy.geocoders
+
 
 pd.set_option('display.width', 32000)
 np.set_printoptions(linewidth=32000)
@@ -31,20 +39,31 @@ api = tw.API(auth, wait_on_rate_limit=True)
 search_words = "#Vaccine -filter:retweets -filter:media -filter:native_video -filter:periscope -filter:vine " \
                "-filter:images -filter:twimg -filter:links -url:amazon"
 date_since = "2018-11-16"
-number_tweets = 20
+number_tweets = 10
 
 
 tweets = tw.Cursor(api.search,
                     q=search_words,
+                    geocode='54.600522,-4.153217,553km',
                     lang="en",
                     since=date_since).items(number_tweets)
 
-print(tweets.__class__)
-
-tweet_data = [[tweet.user.screen_name, tweet.text, tweet.user.location, tweet.retweet_count, tweet.favorite_count] for tweet in tweets]
+tweet_data = [[tweet.user.screen_name, tweet.text, tweet.user.location, tweet.retweet_count, tweet.favorite_count,
+               tweet.place] for tweet in tweets]
 
 tweet_dataframe = pd.DataFrame(data=tweet_data,
-                    columns=['username', 'text', 'location', 'No of retweets', 'likes'])
+                    columns=['username', 'text', 'location', 'No of retweets', 'likes', 'co ords'])
 
 print(tweet_dataframe)
 
+geolocator = Nominatim(user_agent="openmapquests")
+location = geolocator.reverse("51.813431, -0.358474")
+
+
+print(location.raw['address']['county'])
+# print(location.raw.keys())
+# print(location.raw)
+
+'''
+get location using zones from the geocode to make regions each tweet is for.
+'''
